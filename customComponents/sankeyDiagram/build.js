8d64c1692896418c9a1b81796432a885cf8b45f9
Jorge Longoria <jlongoria@ptsteams.com>
2016-10-31 15:18:11 -0500

    sankey custom component

---
+++ customComponents/sankeyDiagram/build.js
@@ -0,0 +1,17 @@
+({
+    optimize: 'none', // Uncomment this property to disable the uglify of the libraries
+    baseUrl: '',
+    paths: {
+			'd3': 'd3.min' ,
+      'sankey': 'sankey',
+			'd3SankeyDiagram': 'd3SankeyDiagram'
+	},
+
+	wrap: {
+        start: "if (typeof define === 'function' && define.amd){}\nelse if (typeof __visualize__ !== 'undefined' &&\ntypeof __visualize__.define === 'function')\n{\n}\n\n(function(root){\n\nvar define = root.define;\n\n",
+        end: "\n\n}(typeof __visualize__ != 'undefined' ? __visualize__ : window));"
+    },
+
+    name: "d3SankeyDiagram",
+    out: "d3SankeyDiagram.min.js"
+})
---
+++ customComponents/sankeyDiagram/d3SankeyDiagram.js
@@ -0,0 +1,122 @@
+define('d3SankeyDiagram',['d3', 'sankey'], function (d3) {
+
+	 return function (instanceData) {
+
+	 	var width = instanceData.width,
+        height = instanceData.height;
+
+		var dataset = instanceData.series[0];
+
+		var margin = {top: 1, right: 1, bottom: 6, left: 1},
+		    width = 960 - margin.left - margin.right,
+		    height = 500 - margin.top - margin.bottom;
+
+		var formatNumber = d3.format(",.2f"),
+		    format = function(d) { return "$" + formatNumber(d); },
+		    color = d3.scale.category20();
+
+		var svg = d3.select("#" + instanceData.id).append("svg")
+				.attr("id", instanceData.id + "svg")
+		    .attr("width", width + margin.left + margin.right)
+		    .attr("height", height + margin.top + margin.bottom)
+		  .append("g")
+		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+	  let nodes = [];
+
+		for(d in dataset) {
+			let source = dataset[d].sources,
+			    target = dataset[d].targets;
+
+			if (nodes.indexOf(source) == -1) nodes.push(source);
+			if (nodes.indexOf(target) == -1) nodes.push(target);
+		}
+
+
+		let data = { "nodes":[], "links":[] };
+
+		for(d in nodes) {
+			data.nodes.push( {"name": nodes[d]} );
+		}
+
+
+		for(d in dataset) {
+			let source = dataset[d].sources,
+			    target = dataset[d].targets,
+					value  = dataset[d].values;
+
+			data.links.push({
+				"source": nodes.indexOf(source),
+				"target": nodes.indexOf(target),
+				"value" : value
+			});
+		}
+
+		var sankey = d3.sankey()
+		    .nodeWidth(15)
+		    .nodePadding(10)
+		    .size([width, height]);
+
+		var path = sankey.link();
+
+		//d3.json("http://localhost:8000/JaspersoftWorkspace/d3SankeyDiagram/energy.json", function(energy) {
+
+		  sankey
+		      .nodes(data.nodes)
+		      .links(data.links)
+		      .layout(32);
+
+		  var link = svg.append("g").selectAll(".link")
+		      .data(data.links)
+		    .enter().append("path")
+		      .attr("class", "link")
+		      .attr("d", path)
+		      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+		      .sort(function(a, b) { return b.dy - a.dy; });
+
+		  link.append("title")
+		      .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+		  var node = svg.append("g").selectAll(".node")
+		      .data(data.nodes)
+		    .enter().append("g")
+		      .attr("class", "node")
+		      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+		    .call(d3.behavior.drag()
+		      .origin(function(d) { return d; })
+		      .on("dragstart", function() { this.parentNode.appendChild(this); })
+		      .on("drag", dragmove));
+
+
+		  node.append("rect")
+		      .attr("height", function(d) { return d.dy; })
+		      .attr("width", sankey.nodeWidth())
+		      .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
+		      .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+		    .append("title")
+		      .text(function(d) { return d.name + "\n" + format(d.value); });
+
+		  node.append("text")
+		      .attr("x", -6)
+		      .attr("y", function(d) { return d.dy / 2; })
+		      .attr("dy", ".35em")
+		      .attr("text-anchor", "end")
+		      .attr("transform", null)
+		      .text(function(d) { return d.name; })
+		    .filter(function(d) { return d.x < width / 2; })
+		      .attr("x", 6 + sankey.nodeWidth())
+		      .attr("text-anchor", "start");
+
+		  function dragmove(d) {
+		    d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+		    sankey.relayout();
+		    link.attr("d", path);
+		  }
+		//});
+
+
+
+	};
+
+});
