de2401b18b0e13720d292aea870b5b4e786135a6
Jorge Longoria <jlongoria@ptsteams.com>
2016-12-20 16:39:13 -0600

    scalability and readability

--- dotChart/js/main.js
+++ dotChart/js/main.js
@@ -1,12 +1,28 @@
+//SVG dimensions
 let rawWidth = 600, rawHeight = 400;
 
-let margin = {top: 80, right: 60, bottom: 100, left: 60},
-    width = rawWidth - margin.left - margin.right,
+let margin = {top: 80, right: 60, bottom: 100, left: 60};
+
+//chart content dimensions
+let width = rawWidth - margin.left - margin.right,
     height = rawHeight - margin.top - margin.bottom;
 
+//label and border adjustments
+let skew = { left: margin.left/2, top:  margin.top/2 };
+
+//percent axis bias
+let bias = {
+  value: 0.02,
+  lower: function() { return +(1 - this.value) },
+  upper: function() { return +(1 + this.value) }
+};
+
+//auto categorical colors
 let colors10 = d3.scale.category10();
 
-//main data
+
+
+//raw data
 let dataset = [
   {category: "1st Six Weeks", series: "2012", measure: "622.7"},
   {category: "2nd Six Weeks", series: "2012", measure: "628.6"},
@@ -30,62 +46,59 @@ let dataset = [
   {category: "6th Six Weeks", series: "2014", measure: "552.4"},
 ];
 
-let datasetMax = d3.max(
-  dataset, function(d) { return +d.measure; }
-);
+//data features
+let data = {
+  max: d3.max(dataset, function(d) { return +d.measure; }),
+  min: d3.min(dataset, function(d) { return +d.measure; }),
 
-let datasetMin = d3.min(
-  dataset, function(d) { return +d.measure; }
-);
-
-let datasetCategories = _.uniq(
-  dataset.map(function(d) { return d.category; })
-);
-
-let axisTolerance = 0.02;
-
-let scale = {
-  min: +(1 - axisTolerance),
-  max: +(1 + axisTolerance)
+  categories: _.uniq(dataset.map(function(d) { return d.category; })),
+  series:     _.uniq(dataset.map(function(d) { return d.series; }))
 };
 
-//measures axis
+
+
+//measure axis
 let x = d3.scale.linear()
-    .domain( [datasetMin*scale.min, datasetMax*scale.max]  )
+    .domain([
+      data.min * bias.lower(),
+      data.max * bias.upper()
+    ])
     .range( [0, width] );
 
-//categories axis
+let xAxis = d3.svg.axis()
+    .orient("bottom")
+    .ticks(width/50)
+    .scale(x);
+
+
+//category axis
 let y = d3.scale.ordinal()
-    .domain( datasetCategories )
+    .domain( data.categories )
     .rangePoints( [0, height] );
 
 let yAxis = d3.svg.axis()
     .orient("left")
     .scale(y);
 
-let xAxis = d3.svg.axis()
-    .orient("bottom")
-    .scale(x);
 
-let skew = { left: margin.left/2, top:  margin.top/2 };
 
 //main SVG instance
 let svg = d3.select("#dot-chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", "translate(" +margin.left+ "," +margin.top+ ")");
+    .attr("transform", translate(margin.left, margin.top));
 
 //y-axis
 svg.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(" +skew.left+ ", " +( -skew.top/2 )+ ")")
+    .attr("transform", translate(skew.left, -skew.top/2))
     .call(yAxis);
 
 //x-axis
 svg.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(" +skew.left+ ", "+height+")")
+    .attr("transform", translate(skew.left, height))
     .call(xAxis);
 
 //border
@@ -98,15 +111,15 @@ svg.append("rect")
 
 //guidelines
 let guidelines = svg.selectAll(".guidelines")
-    .data(datasetCategories).enter();
+    .data( data.categories ).enter();
 
-for(let k=0; k < width; k+=6) {
+for(let x=0, step=Math.max(width/100, 5); x < width; x+=step) {
   guidelines.append("circle")
     .attr("class", "guidelines")
-    .attr("cy", function(d, i) { return y(datasetCategories[i]); })
-    .attr("cx", k)
+    .attr("cy", function(d, i) { return y( data.categories[i] ); })
+    .attr("cx", x)
     .attr("r", 0.5)
-    .attr("transform", "translate(" +skew.left+ ", " +( -skew.top/2 )+ ")");
+    .attr("transform", translate(skew.left, -skew.top/2));
 }
 
 //legend
@@ -122,7 +135,16 @@ svg.selectAll(".dot")
     .attr("r", 3)
     .attr("stroke", function(d) { return colors10(d.series); } )
     .attr("stroke-width", "2px")
-    .attr("transform", "translate(" +skew.left+ ", " +(-skew.top/2)+ ")")
+    .attr("transform", translate(skew.left, -skew.top/2))
  .append("title").text(
    function(d) { return d.category + " - " + d.measure; }
  );
+
+
+
+//helper methods
+function translate(x, y) {
+  return "translate({x}, {y})"
+          .replace("{x}", x)
+          .replace("{y}", y);
+}
